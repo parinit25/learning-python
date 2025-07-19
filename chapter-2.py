9b626e11041e65010536182c5b004a5bc8afef85
Parinit <parinit.singh06@gmail.com>
2025-07-19 18:57:46 +0530

    chapter 2 practice set completed

--- chapter-2.py
+++ chapter-2.py
@@ -6,7 +6,7 @@
 # d= True              # d is a boolean variable
 # e = None             # e is a none type variable
 
-
+#----------------------------------
 # Rules for defining variable names
 #----------------------------------
 
@@ -19,12 +19,53 @@
 
 # parinit, one8 , _parinit , etc.
 
-
+#---------------------------------
 # Operators in Python
+#---------------------------------
 
 # 1. Arithmetic operators: +, -, *, / etc.
 # 2. Assignment operators: =, +=, -= etc.
 # 3. Comparison oprators: ==, >, >=, <, != etc. 
 # 4. Logical operators: and, or, not.
 
+#---------------------------------
+# Type() function and TYPECASTING
+#---------------------------------
 
+a = 31 # type = <class 'int'>
+print(type(a))
+
+b = 'Parinit'
+print(type(b)) # <class 'str'>
+
+c = 31.2
+print(type(c)) # <class 'float'>
+
+d = False
+print(type(d)) # <class 'bool'>
+
+e = None
+print(type(e)) # <class 'NoneType'>
+
+
+# There are also functions like int() float() and str()
+# that are used to convert the types required that the
+# conversion is legal
+
+a = "31.2"
+b = float(a)
+print(type(b)) # <class 'float'>
+
+
+# ----------------
+# Input function
+# ----------------
+
+
+# By default input function takes string as input so to perform mathematical operations
+# we need to convert them to int by using the conversion functions like int(), float(0)
+
+a = int(input("Enter number 1"))
+b = int(input("Enter number 2")) 
+c = a + b
+print("Sum is",c," Number 1 is:",a," Number 2 is:",b)
---
+++ chapter2-ps.py
@@ -0,0 +1,33 @@
+# ----------------
+# Q.1 Sum of 2 numbers
+# ----------------
+
+a = 6
+b = 5
+c = a + b
+print("Sum is",c)
+
+# ----------------
+# Q.2 WAP to find out the remainder when the number is divided by z
+# ----------------
+
+a = 20
+z = 4
+rem = 25 % 4
+print("Remainder is =>",rem)
+
+# ----------------
+# Q.3 Check type of variable using input() function
+# ----------------
+
+a = input("Enter the value of a")
+print(type(a)) # <class 'str'>
+
+# ----------------
+# Q.4 Check if given no. a is greater than b or not
+# ----------------
+
+a = 10
+b = 20
+print(a > b,b > a) # False True
+
