b475719a896136f9098e83c717bb9f87e8b64548
Parinit <parinit.singh06@gmail.com>
2025-07-19 18:25:50 +0530

    chapter 2

---
+++ chapter-2.py
@@ -0,0 +1,30 @@
+# Variables and data-types
+
+# a = 1                # a is an integer
+# b = 5.22             # b is a floating point number
+# c ="Parinit"         # c is a string
+# d= True              # d is a boolean variable
+# e = None             # e is a none type variable
+
+
+# Rules for defining variable names
+#----------------------------------
+
+# 1. A variable name can contain alphabets, digits and underscores.
+# 2. Variable name can only start with alphabets and underscore.
+# 3. A variable name can't start with a digit.
+# 4. No white space is allowed in a variable name.
+
+# Examples of some variable names are :
+
+# parinit, one8 , _parinit , etc.
+
+
+# Operators in Python
+
+# 1. Arithmetic operators: +, -, *, / etc.
+# 2. Assignment operators: =, +=, -= etc.
+# 3. Comparison oprators: ==, >, >=, <, != etc. 
+# 4. Logical operators: and, or, not.
+
+
